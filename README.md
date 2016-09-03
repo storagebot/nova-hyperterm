056fd09ca2a2f978a2cd2c697a0ed0ea3b57361e
Trevor D. Miller <trevordmiller@icloud.com>
2016-09-02 23:38:51 -0600

    Add color scheme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nova-hyperterm
+
 Nova HyperTerm color scheme
+
+# Install
+
+Add `nova-hyperterm` to the plugins array in your `~/.hyperterm.js`.
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const novaColors = require('nova-colors')
+
+exports.decorateConfig = config => Object.assign({}, config, {
+  cursorColor: novaColors.bright.white,
+  foregroundColor: novaColors.normal.white,
+  backgroundColor: novaColors.normal.black,
+  borderColor: novaColors.normal.black,
+  colors: {
+    black: novaColors.normal.black,
+    red: novaColors.normal.red,
+    green: novaColors.normal.green,
+    yellow: novaColors.normal.yellow,
+    blue: novaColors.normal.blue,
+    magenta: novaColors.normal.magenta,
+    cyan: novaColors.normal.cyan,
+    white: novaColors.normal.white,
+    lightBlack: novaColors.bright.black,
+    lightRed: novaColors.bright.red,
+    lightGreen: novaColors.normal.green,
+    lightYellow: novaColors.normal.yellow,
+    lightBlue: novaColors.normal.blue,
+    lightMagenta: novaColors.bright.magenta,
+    lightCyan: novaColors.normal.cyan,
+    lightWhite: novaColors.bright.white,
+  },
+  termCSS: `
+    .cursor-node {
+      opacity: 0.5 !important;
+    }
+  `,
+  css: `
+    .tab_first {
+      margin-left: 0 !important;
+    }
+    .tab_tab {
+      background-color: ${novaColors.decoration.medium} !important;
+    }
+    .tab_tab.tab_active {
+      background-color: ${novaColors.normal.black} !important;
+      border: none !important;
+    }
+    .tab_tab.tab_active::before {
+      border: none !important;
+    }
+    .tab_text {
+      border: none !important;
+    }
+    .tabs_title,
+    .tab_textInner {
+      font-size: 14px !important;
+      font-weight: bold !important;
+    }
+  `,
+})
